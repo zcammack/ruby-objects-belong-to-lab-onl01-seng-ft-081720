da7be6e51b21c87baac73a6e536adb4d576e0093
Zackary Cammack <zack.cammack@gmail.com>
2020-09-01 13:34:57 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+class Post
+
+  attr_accessor :title, :author
+
+  def initialize
+    @title = title
+    @author = author
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+
+  attr_accessor :title, :artist
+
+  def initialize
+    @title = title
+    @artist = artist
+  end
+
+end
